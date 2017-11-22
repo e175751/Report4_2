e60ecc0a1356e8c0223b90202229afe86ec0ac76
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2017-11-22 17:11:19 +0900

    second commit

--- src/main/jp/ac/uryukyu/ie/e175751/Main.java
+++ src/main/jp/ac/uryukyu/ie/e175751/Main.java
@@ -1,9 +1,12 @@
 package jp.ac.uryukyu.ie.e175751;
 
 public class Main {
-        public static void main(String[] arg){
-            String str = null;
-            System.out.println(str.length());
-
+        public static void main(String[] args){
+            try {
+                String str = null;
+                System.out.println(str.length());
+            }catch(NullPointerException w) {
+                System.out.println(w.getMessage());
+            }
         }
 }
