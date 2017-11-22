7ac67048917e0b9d0a973ea1e557f0f603ff715c
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2017-11-22 15:27:11 +0900

    first commit

---
+++ src/main/jp/ac/uryukyu/ie/e175751/Main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e175751;
+
+public class Main {
+        public static void main(String[] arg){
+            String str=null;
+            System.out.println(str.length());
+        }
+}
