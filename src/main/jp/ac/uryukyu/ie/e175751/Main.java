005434894d2bb64a2fbcd66ecbb5b2ae223a5306
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2017-11-22 19:00:57 +0900

    third commit

--- src/main/jp/ac/uryukyu/ie/e175751/Main.java
+++ src/main/jp/ac/uryukyu/ie/e175751/Main.java
@@ -3,10 +3,12 @@ package jp.ac.uryukyu.ie.e175751;
 public class Main {
         public static void main(String[] args){
             try {
-                String str = null;
-                System.out.println(str.length());
+                String str = "3．14";
+                double value = Double.parseDouble(str);
+                System.out.println();
             }catch(NullPointerException w) {
                 System.out.println(w.getMessage());
             }
         }
+
 }
